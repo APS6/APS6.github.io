cafca164305de9ff14ef71df5f12c7e4547bf714
APS6 <120901252+APS6@users.noreply.github.com>
2023-02-19 14:42:58 +0530

    Delete portfolio directory

--- portfolio/js/resp.js
+++
@@ -1,7 +0,0 @@
-burger = document.querySelector('.burger')
-sidebar = document.querySelector('.sidebar')
-
-burger.addEventListener('click', () => {
-    sidebar.classList.toggle("active");
-    burger.classList.toggle("active")
-})
