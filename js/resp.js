3828a60ddbb69b882f3c19b518109fb28d1d8359
APS6 <120901252+APS6@users.noreply.github.com>
2023-02-21 18:13:39 +0530

    Add files via upload

--- js/resp.js
+++ js/resp.js
@@ -5,3 +5,18 @@ burger.addEventListener('click', () => {
     sidebar.classList.toggle("active");
     burger.classList.toggle("active")
 })
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+         } else {
+             entry.target.classList.remove('show');
+         }
+     });
+});
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=> observer.observe(el));
