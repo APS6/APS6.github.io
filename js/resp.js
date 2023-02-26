9c6ee9a72998f05d9e5caa7a5864091fd78f46c2
APS6 <120901252+APS6@users.noreply.github.com>
2023-02-26 13:37:27 +0530

    Add files via upload

--- js/resp.js
+++ js/resp.js
@@ -13,10 +13,44 @@ const observer = new IntersectionObserver((entries) => {
         console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-         }
-     });
+        }
+    });
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el)=> observer.observe(el));
+hiddenElements.forEach((el) => observer.observe(el));
 
+// break website
+press = document.querySelector('#press')
+bonk = document.querySelector('#bonk')
+boom = document.querySelector('#boom')
+tex = document.querySelector('#mess')
+
+press.addEventListener('click', () => {
+    boom.classList.add("boom");
+    bonk.classList.add("blady");
+    tex.classList.add("show");
+})
+
+// fix website
+safe = document.querySelector('#recover')
+bonk = document.querySelector('#bonk')
+tex = document.querySelector('#mess')
+safe.addEventListener('click', () => {
+    bonk.classList.remove("blady");
+    tex.classList.remove("show");
+    boom.classList.remove("boom");
+
+})
+
+// blob
+const blob = document.getElementById("blob");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 3000, fill: "forwards" });
+}
